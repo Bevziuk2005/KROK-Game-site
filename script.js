36e640c372d896c78cd8b4a0956bc0b256cab1e3
ArtemGit1 <131163813+ArtemGit1@users.noreply.github.com>
2025-04-08 11:36:06 +0300

    Пофіксив хедер трохи, зробив більш менш норм модалку, пофіксив картки команд в індексі
    +

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
-const burgerIcon = document.getElementById('burger-icon');
-const modal = document.getElementById('modal');
-
-
-burgerIcon.addEventListener('click', () => {
-    modal.style.display = 'block';
+document.getElementById('burger-icon').addEventListener('click', function () {
+    const modal = document.getElementById('modal');
+    modal.classList.toggle('active'); // Додаємо/забираємо клас active
 });
 
-
-modal.addEventListener('click', (e) => {
-    if (e.target === modal) {
-        modal.style.display = 'none';
+document.getElementById('modal').addEventListener('click', function (event) {
+    if (event.target === this) {
+        this.classList.remove('active'); // Закриття при кліку поза контентом
     }
 });
+
+
+const quotes = [
+    '«Не думкам треба вчити, а думати» – Іммануїл Кант',
+    '«7 вересня відзначають День кіберспорту в Україні Саме в цей день кіберспорт був офіційно визнаний видом спорту в Україні завдяки Федерації Кіберспорту України (UESF)',
+    '«З 18 січня по 28 березня 2021 року в Києві відбувся перший Національний Кубок України з кіберспорту',
+    '«Історія цифрофого спорту почалася з таких ігор, як  Quake, які дозволяли грати в мережі через локальну мережу або інтернет',
+  ];
+
+  const randomIndex = Math.floor(Math.random() * quotes.length);
+  const randomQuote = quotes[randomIndex];
+
+  document.getElementById('quote').textContent = randomQuote;
