8fbe66a84a0e963972a65f0e300186cf97b86075
ArtemGit1 <131163813+ArtemGit1@users.noreply.github.com>
2025-05-21 13:18:48 +0300

    Додав зміну цитати кожних 10 секунд

--- script.js
+++ script.js
@@ -1,23 +1,39 @@
 document.getElementById('burger-icon').addEventListener('click', function () {
     const modal = document.getElementById('modal');
-    modal.classList.toggle('active'); // Додаємо/забираємо клас active
+    modal.classList.toggle('active');
 });
 
 document.getElementById('modal').addEventListener('click', function (event) {
     if (event.target === this) {
-        this.classList.remove('active'); // Закриття при кліку поза контентом
+        this.classList.remove('active');
     }
 });
 
 
 const quotes = [
-    '«Не думкам треба вчити, а думати» – Іммануїл Кант.',
-    '7 вересня відзначають День кіберспорту в Україні. Саме в цей день кіберспорт був офіційно визнаний видом спорту в Україні завдяки Федерації Кіберспорту України (UESF)',
-    'З 18 січня по 28 березня 2021 року в Києві відбувся перший Національний Кубок України з кіберспорту.',
-    'Історія цифрового спорту почалася з таких ігор, як Quake, які дозволяли грати в мережі через локальну мережу або інтернет.',
-  ];
+  '«Не думкам треба вчити, а думати» – Іммануїл Кант.',
+  '7 вересня відзначають День кіберспорту в Україні. Саме в цей день кіберспорт був офіційно визнаний видом спорту в Україні завдяки Федерації Кіберспорту України (UESF).',
+  'З 18 січня по 28 березня 2021 року в Києві відбувся перший Національний Кубок України з кіберспорту.',
+  'Історія цифрового спорту почалася з таких ігор, як Quake, які дозволяли грати в мережі через локальну мережу або інтернет.',
+];
 
-  const randomIndex = Math.floor(Math.random() * quotes.length);
-  const randomQuote = quotes[randomIndex];
+const quoteElement = document.getElementById('quote');
+let currentQuoteIndex = 0;
 
-  document.getElementById('quote').textContent = randomQuote;
+
+function changeQuote() {
+
+
+
+  setTimeout(() => {
+    currentQuoteIndex = (currentQuoteIndex + 1) % quotes.length; 
+    quoteElement.textContent = quotes[currentQuoteIndex];
+
+  }, 1000);
+}
+
+
+setInterval(changeQuote, 10000);
+
+
+quoteElement.textContent = quotes[currentQuoteIndex];
